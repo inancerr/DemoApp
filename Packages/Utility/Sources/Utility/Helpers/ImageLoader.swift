719597b18d4d3c364a42701184eea6b9bc25a0b8
İnanç Er <inancerr@gmail.com>
2021-05-11 23:59:24 +0300

    Image cache variable type has been converted to NSCache

--- Packages/Utility/Sources/Utility/Helpers/ImageLoader.swift
+++ Packages/Utility/Sources/Utility/Helpers/ImageLoader.swift
@@ -10,26 +10,26 @@ import class Foundation.URLSessionConfiguration
 import class Foundation.DispatchQueue
 import struct Foundation.Data
 import struct Foundation.URL
+import class Foundation.NSCache
+import class Foundation.NSString
 import class UIKit.UIImage
 
 class ImageLoader {
-    
     static let shared = ImageLoader()
-    
-    var cache: [URL: Data] = [:]
+    var cache = NSCache<NSString, UIImage>()
     
     func loadImage(_ url: String, completion: @escaping (UIImage) -> ()) {
         DispatchQueue.global(qos: .userInitiated).async { [weak self] in
             guard let _url = URL(string: url) else { return }
-            var data: Data?
-            if let cacheData = self?.cache[_url] {
-                data = cacheData
-            } else if let _data = try? Data(contentsOf: _url) {
-                data = _data
-                self?.cache.updateValue(_data, forKey: _url)
+            if let image = self?.cache.object(forKey: NSString(string: url)) {
+                DispatchQueue.main.async {
+                    completion(image)
+                }
+                return
             }
-            guard let _data = data,
+            guard let _data = try? Data(contentsOf: _url),
                   let image = UIImage(data: _data) else { return }
+            self?.cache.setObject(image, forKey: NSString(string: url))
             DispatchQueue.main.async {
                 completion(image)
             }
